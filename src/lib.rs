5d7eedfb1dacf99aa15d529b9d13ce4a925169d8
Josh Abraham <sinisterpatrician@gmail.com>
2018-12-27 22:14:45 -0500

    Basic parsing working

--- src/lib.rs
+++ src/lib.rs
@@ -11,18 +11,48 @@ pub enum Permissions {
     Shared,
     Private,
 }
-
+/*
 pub struct Map {
-    pub base: *const u8,
-    pub size: usize,
-    pub perms: Permissions,
+    pub base: usize,
+    pub ceiling: usize,
+    pub perms: String,
     pub offset: usize,
     pub dev_major: usize,
     pub dev_minor: usize,
     pub inode: usize,
     pub pathname: String,
 }
+*/
+#[derive(Debug)]
+pub struct Map {
+    pub base: String,
+    pub ceiling: String,
+    pub perms: String,
+    pub offset: String,
+    pub dev_major: String,
+    pub dev_minor: String,
+    pub inode: String,
+    pub pathname: String,
+}
 
+named!(parse_map<&str, Map>,
+    do_parse!(
+        base: map!(take_until!("-"), String::from)      >>
+        take!(1)                                        >>
+        ceiling: map!(take_until!(" "), String::from)   >>
+        take!(1)                                        >>
+        perms: map!(take_until!(" "), String::from)     >>
+        take!(1)                                        >>
+        offset: map!(take_until!(" "), String::from)    >>
+        dev_major: map!(take_until!(":"), String::from) >>
+        dev_minor: map!(take_until!(" "), String::from) >>
+        inode: map!(take_until!(" "), String::from)     >>
+        pathname: map!(take_until!("\n"), String::from) >>
+        (Map {base: base.into(), ceiling: ceiling.into(), perms: perms.into(), offset: offset.into(), dev_major: dev_major.into(), dev_minor: dev_minor.into(), inode: inode.into(), pathname: pathname.into()})
+    )
+);
+
+/*
 pub fn maps(pid: pid_t) -> Result<Map> {
     Ok( Map {
         base: 0x00000 as *const u8,
@@ -35,12 +65,15 @@ pub fn maps(pid: pid_t) -> Result<Map> {
         pathname: String::from("hello"),
     })
 }
+*/
 
 #[cfg(test)]
 mod tests {
+    use crate::*;
     #[test]
     fn it_works() {
-        let input = b"55e8d4153000-55e8d416f000 r-xp 00000000 08:02 9175073                    /bin/dash";
-        assert_eq!(2 + 2, 4);
+        let input = "55e8d4153000-55e8d416f000 r-xp 00000000 08:02 9175073                    /bin/dash\n";
+        let res = parse_map(input);
+        println!("{:?}", res);
     }
 }
