728e9f36ee7bb771e7715620b58bcd91c5361fc6
Joshua Abraham <sinisterpatrician@gmail.com>
2020-09-20 10:14:33 -0400

    Fix compile erros on rust

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,3 @@
-#![feature(ptr_wrapping_offset_from)]
 #![feature(test)]
 
 extern crate test;
@@ -38,7 +37,7 @@ impl error::Error for Error {
         }
     }
 
-    fn cause(&self) -> Option<&error::Error> {
+    fn cause(&self) -> Option<&dyn error::Error> {
         None
     }
 }
